270dd64d6bcc2d0a8f1a74c34c560b7bf14755c7
Ashraf Hadden <ashrafhadden@gmail.com>
2020-09-23 16:06:36 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ powercfg.exe /hibernate off
 :: Install Chocolatey
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+:: Apps
 choco install signal
 choco install discord
 choco install caprine
