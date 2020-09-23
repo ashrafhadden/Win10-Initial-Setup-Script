8f730ae7867718be8cf73f82da3197d462b9c7ed
Ashraf Hadden <ashrafhadden@gmail.com>
2020-09-23 16:22:55 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,10 @@
+:: Auto-Login
+echo setup.bat BEGIN
+set /p setupbatvar1="Enter password "
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d domainname /f
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d Administrator /f
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d %setupbatvar1% /f
 :: Disable Hiberation & delete hiberfil.sys (saves approx. 1-5gb)
 powercfg.exe /hibernate off
 :: Choco
@@ -17,3 +24,4 @@ start chrome https://signup.na.leagueoflegends.com/en/signup/redownload
 start chrome https://valorant.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.live.na.exe
 start chrome https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
 start chrome https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP
+echo setup.bat END
