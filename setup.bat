2ec28abc7974c2692ae4ab2fe70edd6b50d80070
Ashraf Hadden <ashrafhadden@gmail.com>
2020-09-23 16:07:04 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 :: Disable Hiberation & delete hiberfil.sys (saves approx. 1-5gb)
 powercfg.exe /hibernate off
-:: Install Chocolatey
+:: Choco
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 :: Apps
